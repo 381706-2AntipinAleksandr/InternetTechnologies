7ff0b46537cfc75f160aabc4ef3c8604342d75c3
aleksandr.antipin.1999@mail.ru <antipin.aleksandr.1999@mail.ru>
2019-10-22 16:32:20 +0300

    working script

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
-/*let stack = {
-    length: 0,
-    mem: [],
-    push: function(elem){
-        this.mem.push()
-    }
-};*/
-
-
 
 function treeOfTags(idOfTag){
     const rootElem = document.getElementById(idOfTag);
     let currentElem;
-    let output = document.getElementById('output');
+    let outputStart = document.getElementById('output');
     let masElems = [];
+    let masElems2 = [];
     if (rootElem.firstElementChild != undefined){
         currentElem = rootElem.firstElementChild;
         masElems.push(rootElem.firstElementChild);
-        while(currentElem){
-            //output.lastChild = "li";
-            if (currentElem.firstElementChild != undefined){
-                currentElem = currentElem.firstElementChild;
-                masElems.push(currentElem.firstElementChild);
-            } else if (currentElem.nextElementSibling != undefined) {
-                currentElem = currentElem.nextElementSibling;
+        while(masElems[masElems.length - 1] != undefined){
+            currentElem = masElems.pop();
+            if (currentElem.parentElement === rootElem){
+                masElems2.push(currentElem);
+            }
+            if (currentElem.nextElementSibling != undefined){
                 masElems.push(currentElem.nextElementSibling);
-            } else
-                continue;
+            }
+            if (currentElem.firstElementChild != undefined) {
+                masElems.push(currentElem.firstElementChild);
+            }
         }
     }
-
+    building(masElems2, outputStart);
 }
 
+function building(mas, start){
+    let outputCurrentUL = document.createElement('ul');
+    let outputStart = start;
+    outputStart.append(outputCurrentUL);
+    for(let i = 0; i < mas.length; i++){
+        let outputCurrentLIO = document.createElement('li');
+        outputCurrentLIO.innerHTML = 'tag - ' + mas[i].tagName + ', id - ' + mas[i].id;
+        outputCurrentUL.append(outputCurrentLIO);
+        if(mas[i].firstElementChild != undefined){
+            let masRec = [];
+            for (let j = 0; j < mas[i].children.length; j++){
+                masRec[j] = mas[i].children[j]
+            }
+            building(masRec, outputCurrentUL);
+        }
+    }
+}
